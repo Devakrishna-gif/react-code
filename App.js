536f5f343706d80f70688717459cd056109a239e
Devakrishna-gif <raminenidevakrishna@gmail.com>
2024-07-29 16:12:04 +0530

    code-3

--- App.js
+++ App.js
@@ -1,6 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-let heading = React.createElement('h1', { id: 'parent', xyz: 'abc' }, [React.createElement('div', { id: 'child' }, [React.createElement('h1', {}, 'This is h1 tag'), React.createElement('h2', {}, 'This is h2 tag')]), React.createElement('div', { id: 'child2' }, [React.createElement('h1', {}, 'This is h1 tag'), React.createElement('h2', {}, 'This is h2 tag')])]);
-let root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+let money = 20000;
+let name = "Devakrishna";
+
+let JsxHeading =  (
+  <>
+  <p>{name}</p>
+  <h1 id="heading">This is React Element using JSX</h1>
+</>
+);
+
+let FunctionalHeading = ()=>{
+  return (
+    <>
+    <p>{money}</p>
+    {JsxHeading}
+    <h1>Functional Component using React</h1>
+    </>
+  )
+}
+
+let root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<FunctionalHeading/>);
